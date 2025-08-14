e533251335df3a1cefa86fffadc1f53e7df0f20b
Jahidul Islam <ionzahid0987@gmail.com>
2025-08-14 22:24:18 +0600

    Create views.py

---
+++ backend/users/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, "Error creating account")
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid credentials")
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
