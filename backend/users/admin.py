a1ee27a1bdbd44bb5bda079e92aebf9fcce33859
Jahidul Islam <ionzahid0987@gmail.com>
2025-08-14 22:22:50 +0600

    Create admin.py

---
+++ backend/users/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('full_name',)}),
+    )
